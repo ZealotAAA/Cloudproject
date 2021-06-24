a83719ad377b56090c1ea99787ea211ec37766c7
Xu <xyp4913@gmail.com>
2021-06-24 16:56:49 +0200

    heroku update

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: "app",
+    script: "./app.js",
+    instances: "max",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    },
+	env_heroku: {
+		NODE_ENV: "heroku",
+	}
+  }]
+}
